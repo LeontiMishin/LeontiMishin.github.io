d2da5057ee2d66cef1fb8520ac8e3e0f3d2b1f92
Leonti <leonti.gold@gmail.com>
2022-11-22 09:06:11 +0200

    node Simple Web-server with pages

--- node/index.js
+++ node/index.js
@@ -1,35 +1,56 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
-    res.writaHead(200, {
-        'Content-Type': 'text/html'
-    })
-    res.end(`
-        <h1>Form</h1>
-        <form method="post" action="/">
-            <input name="title" type="text" />
-            <button type="submit">Send</button>
-        </form>
-    `)
-}   else if (req.method === 'POST') {
-    const body = []
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-    })
+        res.writaHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
 
-    req.on('data', data => {
-        dody.push(Buffer.from(data))
-    })
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
 
-    req.on('end', () => {
-        const message = body.toString().split('=')[1]
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
 
-        res.end(`
-            <h1>Ваше сообщение: ${message}</h1>
-        `)
-    })
-}
+                    res.end(content)
+                }
+            )
+        }
+    }else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', data => {
+            dody.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+
+            res.end(`
+                <h1>Ваше сообщение: ${message}</h1>
+            `)
+        })
+    }
 })
 
 server.listen(3000, () => {
