57991eff2ecaa3c16c3f459898b5cc1b5429c25e
Leonti <leonti.gold@gmail.com>
2022-11-22 09:39:02 +0200

    node Simple API server

--- node/index.js
+++ node/index.js
@@ -32,6 +32,17 @@ const server = http.createServer((req, res) => {
                     res.end(content)
                 }
             )
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json'
+            })
+
+            const users = [
+                {name: 'Vasya', age: 20},
+                {name: 'Elena', age: 23}
+            ]
+
+            res.end(JSON.stringify(users))
         }
     }else if (req.method === 'POST') {
         const body = []
